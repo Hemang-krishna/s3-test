0324d61e8f88a5cdaee6b15e89dbf7744cd1865a
Hemang-krishna <krishnachaitanyalagadapatihema@gmail.com>
2026-03-19 18:40:16 +0530

    backend and s3-frontend changes

--- s3-test-frontend/vite.config.ts
+++ s3-test-frontend/vite.config.ts
@@ -5,10 +5,10 @@ import babel from '@rolldown/plugin-babel'
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   server: {
-    port: 5173,
+    port: 5174,
     proxy: {
       '/api': {
-        target: 'http://localhost:7000',
+        target: 'http://localhost:7100',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
